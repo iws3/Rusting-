4400c413fe0dfe8a8d3bdc7f6a19c37205004fb9
iws-seed <iwstechnical7@gmail.com>
2026-03-17 08:36:21 +0100

    working and learning about pattern matching

--- rust-exercises/students/gita/part 2/07_pattern_matching/part.rs
+++ rust-exercises/students/gita/part 2/07_pattern_matching/part.rs
@@ -18,3 +18,30 @@ fn main() {
     let coin=Coin::Dime;
     println!("Value: {}", value_in_cents(coin));
 }
+
+
+
+enum Players {
+    Messi,
+    Ronaldo,
+    Neymar,
+    Mbappe
+}
+
+
+fn player_number(player:Player)->u8 {
+    match playder {
+        Player::Messi=>10,
+        Player::Ronaldo=>7,
+        Neymar=>11,
+        Mbappe=>7
+    }
+}
+
+
+fn main() {
+    let player=Player::Messi;
+    println!("Player Number: {}", player_number(player));
+}
+
+
