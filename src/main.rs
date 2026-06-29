88082b8f523052c0f71c300f7f0eae752397b47f
iws-seed <iwstechnical7@gmail.com>
2026-06-29 10:39:04 +0100

    added second case , much more readable than first case, using turples

--- src/main.rs
+++ src/main.rs
@@ -1064,13 +1064,24 @@
 
 // rewrite the program below using structs
 
-fn main() {
-    let width1=30;
-    let height1=50;
-    println!("The area of a rectangle is {} square pixels.", area(width1, height1))
+// fn main() {
+//     let width1=30;
+//     let height1=50;
+//     println!("The area of a rectangle is {} square pixels.", area(width1, height1))
+// }
+
+
+// fn area(width:u32, height:u32)->u32{
+// width * height
+// }
+
+// improve the readabilty by grouping this two variables together using tuples
+
+fn main () {
+    let rect=(30, 50);
+    println!("The value of our area calculated is : {}: ", area(rect));
 }
 
-
-fn area(width:u32, height:u32)->u32{
-width * height
+fn area(dimensions: (u32, u32))->u32 {
+    dimensions.0 * dimensions.1
 }
